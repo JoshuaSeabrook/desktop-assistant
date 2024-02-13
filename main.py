8c6fb6267a0f25c89adefa46c7be11ca70b51efe
Joshua Seabrook <joshua.d.seabrook@gmail.com>
2024-02-13 17:26:06 +0000

    FIX: Update main.py to correctly call controller run method

--- main.py
+++ main.py
@@ -2,8 +2,4 @@ from controllers.application_controller import ApplicationController
 
 if __name__ == '__main__':
     controller = ApplicationController()
-    while True:
-        user_input = controller.view.get_user_input()
-        if user_input.lower() == 'exit':
-            break
-        controller.process_input(user_input)
+    controller.run()
