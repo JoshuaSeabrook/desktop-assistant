3a268e0477881ecc43dcf11fb3769be3e8e8ffab
Joshua Seabrook <joshua.d.seabrook@gmail.com>
2024-02-14 16:33:57 +0000

    FEAT: Add InputProcessor class to allow asynchronous response generation

--- controllers/application_controller.py
+++ controllers/application_controller.py
@@ -1,10 +1,22 @@
 import sys
 
+from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal, QThread, QMetaObject, Q_ARG
 from PyQt5.QtWidgets import QApplication
 
 from models.response_generator import ResponseGenerator
 from views.user_interface import MainWindow
 
+class InputProcessor(QObject):
+    finished = pyqtSignal(str)
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    @pyqtSlot(str)
+    def process(self, user_input):
+        response = self.model.get_response(user_input)
+        self.finished.emit(response)
 
 class ApplicationController:
     def __init__(self):
@@ -12,10 +24,20 @@ class ApplicationController:
         self.model = ResponseGenerator()
         self.view = MainWindow(self)
 
+        self.thread = QThread()
+        self.inputProcessor = InputProcessor(self.model)
+        self.inputProcessor.moveToThread(self.thread)
+        self.inputProcessor.finished.connect(self.update_ui_with_response)
+        self.thread.start()
+
     def process_input(self, user_input):
+        self.view.sendButton.setEnabled(False)
         self.view.display_message(f"You: {user_input}")
-        response = self.model.get_response(user_input)
+        QMetaObject.invokeMethod(self.inputProcessor, 'process', Q_ARG(str, user_input))
+
+    def update_ui_with_response(self, response):
         self.view.display_message(f"Assistant: {response}")
+        self.view.sendButton.setEnabled(True)
 
     def run(self):
         self.view.show()
